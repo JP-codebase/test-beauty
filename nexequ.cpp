af509767c402073179c46b7c71926a18e8ab5c9e
JP-codebase <gianpaolosantarsiero@gmail.com>
2025-03-18 18:56:05 +0100

    It works, but generates multiple partitions

--- nexequ.cpp
+++ nexequ.cpp
@@ -12,16 +12,40 @@ bool isSingleton(unsigned int index, unsigned int* partition) {
 }
 
 // TODO : genereate only 1 element of the sequence
-void nexeq(unsigned int* parition, unsigned int setSize) {
+// The function generates the next partition given the current
+// It returns if the program reaches its end (`true` == exeuction finished`)
+bool nexequ(unsigned int* partition, unsigned int setSize) {
   // 1: Find highest non singleton element `active`
-
-  int active;
-  for (active = setSize - 1; active >= 0; active--) {
+  unsigned int active;
+  for (active = setSize - 1; active >= 1; active--) {
+    if (!isSingleton(active, partition)) break;
   }
 
+  if (active == 0) return true;
+
   // 2: Move `active` to the next subset class
+  partition[active]++;
+
+  // Print partition
+  for (unsigned int i = 0; i < setSize; i++) {
+    std::cout << partition[i] << ' ';
+  }
+  std::cout << std::endl;
 
   // 3: Move alle elements ]`active`, `setSize`[ in the first subset class
+  if (active != (setSize - 1)) {
+    for (unsigned int i{active + 1}; i < setSize; i++) {
+      partition[i] = 0;
+    }
+
+    // Print partition
+    for (unsigned int i = 0; i < setSize; i++) {
+      std::cout << partition[i] << ' ';
+    }
+    std::cout << std::endl;
+  }
+
+  return false;
 }
 
 int main() {
@@ -44,6 +68,13 @@ int main() {
   for (unsigned int i = 0; i < setSize; i++) {
     partition[i] = 0;
   }
+
+  unsigned int counter{0};
+  bool completed{false};
+  do {
+    completed = nexequ(partition, setSize);
+    counter++;
+  } while ((counter < 60) && !completed);
 
   // Print
 
