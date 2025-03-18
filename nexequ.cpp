1fd38d5929b23f67348cc3702414ba6d1f16fdc8
JP-codebase <gianpaolosantarsiero@gmail.com>
2025-03-18 17:21:35 +0100

    isSingleton

--- nexequ.cpp
+++ nexequ.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
-int main(){
+// Given an element in the `index` array cell, this function
+// checks if there exists an other element with index `j` such that
+// 0 <= `j` < `index` in the array
 
+bool isSingleton(unsigned int index, unsigned int* partition) {
+  for (int i = index - 1; i >= 0; i--) {
+    if (partition[i] == partition[index]) return false;
+  }
+
+  return true;
+}
+
+int main() {
   unsigned int setSize{0};
   std::cout << "Insert set size: ";
   std::cin >> setSize;
 
   // Initial set composed of `setSize` distiguishable characters
-  
+
   /*unsigned char* set {new unsigned char [setSize]};*/
   /**/
   /*for (int i = 0; i < setSize; i++){*/
@@ -16,22 +27,21 @@ int main(){
   /*}*/
   /*std::cout << std::endl;*/
 
+  unsigned int* partition{new unsigned int[setSize]};
 
-
-  unsigned int* partition {new unsigned int [setSize]};
-
-  for (int i = 0; i < setSize; i++){
-    partition[i] = i;
+  for (int i = 0; i < setSize; i++) {
+    partition[i] = 0;
   }
 
+  std::cout << isSingleton(setSize - 1, partition) << std::endl;
 
   // Print
 
   std::cout << "----------------------------------" << std::endl;
-  for (int i = 0; i < setSize; i++){
+  for (int i = 0; i < setSize; i++) {
     std::cout << partition[i] << ' ';
   }
   std::cout << std::endl;
-  
+
   return 0;
 }
