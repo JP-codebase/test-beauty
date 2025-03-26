6e4f3a50cde34d16412914c629cc382ba0e671b2
JP-codebase <gianpaolosantarsiero@gmail.com>
2025-03-26 16:42:54 +0100

    number_of_partition, execution_time, status_bar

--- nexpar.cpp
+++ nexpar.cpp
@@ -1,8 +1,16 @@
+#include <chrono>
 #include <cmath>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <string>
+
+const double pi{std::acos(-1.0)};
+
+// Number of partition of the integer n
+double number_partition_n(unsigned int n) {
+  return std::exp(pi * std::sqrt(2.0L * n / 3.0L)) / (4 * n * std::sqrt(3.0L));
+}
 
 // nexpar
 // It returns true if the program reaches its end (`true` == exeuction
@@ -150,15 +158,16 @@ int main() {
   std::cout << "Insert an integer > 0: ";
   std::cin >> n;
 
-  /*std::cout << "You are going to generate " << Bell_number(setSize)*/
-  /*          << " partitions. Do you want to continue [Y/n]?" << std::endl;*/
-  /**/
+  const double number_of_partitions{number_partition_n(n)};
+  std::cout << "You are going to generate approximately "
+            << number_of_partitions
+            << " partitions. Do you want to continue [Y/n]?" << std::endl;
 
-  /*char input{};*/
-  /*std::cin >> input;*/
-  /**/
-  /*if (input != 'Y' && input != 'y')*/
-  /*  return 0;*/
+  char input{};
+  std::cin >> input;
+
+  if (input != 'Y' && input != 'y')
+    return 0;
 
   unsigned int *partition{new unsigned int[n]};
   unsigned int *dg_profile{new unsigned int[n]};
@@ -180,31 +189,57 @@ int main() {
   float min_relevance[2]{1, rel};
   float max_relevance[2]{1, rel};
 
-  bool completed{false};
-  unsigned int counter{1};
+  // Variables used for formatting the output to the terminal
+  int status_bar{0};
+  unsigned int ten_percent = number_of_partitions / 10;
   unsigned int width{
       static_cast<unsigned int>(std::ceil(std::log10(static_cast<double>(n)))) +
       1};
+
+  // Time
+  auto start = std::chrono::steady_clock::now();
+  auto time_step1 = start;
+  auto time_step2 = start;
+
+  // Starting the loop
+  bool completed{false};
+  unsigned long int counter{1};
+
+  std::cout << std::endl;
+  std::cout << "----------------------------" << std::endl;
+
   do {
 
-    std::cout << counter << " :\t";
-    for (unsigned int i{0}; i < n; i++) {
-      std::cout << std::setw(width) << partition[i] << ' ';
+    if (counter > (status_bar * ten_percent)) {
+      time_step1 = std::chrono::steady_clock::now();
+      std::cout << status_bar << "0% : "
+                << ((std::chrono::duration_cast<std::chrono::milliseconds>(
+                         (time_step1 - time_step2)))
+                        .count()) /
+                       1000.0
+                << " seconds" << std::endl;
+      time_step2 = time_step1;
+      status_bar++;
     }
+
+    /*std::cout << counter << " :\t";*/
+    /*for (unsigned int i{0}; i < n; i++) {*/
+    /*  std::cout << std::setw(width) << partition[i] << ' ';*/
+    /*}*/
 
     // Print degeneracy profile
-    std::cout << '\t' << "dg. : ";
-    for (unsigned int i = 0; i < n; i++) {
-      std::cout << std::setw(width) << dg_profile[i] << ' ';
-    }
+    /*std::cout << '\t' << "dg. : ";*/
+    /*for (unsigned int i = 0; i < n; i++) {*/
+    /*  std::cout << std::setw(width) << dg_profile[i] << ' ';*/
+    /*}*/
 
     // Print resolution
-    std::cout << '\t' << "res : " << res;
+    /*std::cout << '\t' << "res : " << res;*/
 
     // Print relevance
-    std::cout << '\t' << "rel : " << rel;
+    /*std::cout << '\t' << "rel : " << rel;*/
 
-    std::cout << std::endl;
+    /*std::cout << std::endl;*/
 
     completed = nexpar(partition, n);
     degeneracy_profile(partition, dg_profile, n);
@@ -231,8 +266,26 @@ int main() {
     }
   } while (!completed);
 
+  // Execution time
+  time_step1 = std::chrono::steady_clock::now();
+  std::cout << "100% : "
+            << ((std::chrono::duration_cast<std::chrono::milliseconds>(
+                     (time_step1 - time_step2)))
+                    .count()) /
+                   1000.0
+            << " seconds" << std::endl;
+
+  std::cout << "Execution time : "
+            << ((std::chrono::duration_cast<std::chrono::milliseconds>(
+                     (time_step1 - start)))
+                    .count()) /
+                   1000.0
+            << " seconds" << std::endl;
+
+  std::cout << std::endl;
   std::cout << "----------------------------" << std::endl;
 
+  // Max and Min Relevance and Resolution
   std::cout << std::setw(20) << "Min resolution : " << std::setw(10)
             << min_resolution[1] << std::setw(20) << std::setw(20)
             << "position : " << std::setw(10) << min_resolution[0] << std::endl;
@@ -240,7 +293,7 @@ int main() {
             << max_resolution[1] << std::setw(20)
             << "position : " << std::setw(10) << max_resolution[0] << std::endl;
 
-  std::cout << "---------" << std::endl;
+  std::cout << "----------------------------" << std::endl;
   std::cout << std::setw(20) << "Min relevance : " << std::setw(10)
             << min_relevance[1] << std::setw(20) << std::setw(20)
             << "position : " << std::setw(10) << min_relevance[0] << std::endl;
