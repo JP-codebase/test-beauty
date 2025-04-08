ecdd8e96909c7cb511567ec95e41ee221d475c29
JP-codebase <gianpaolosantarsiero@gmail.com>
2025-04-08 10:05:41 +0200

    energy functions in quantifying information header and cpp

--- nexpar/quantifying_information.h
+++ nexpar/quantifying_information.h
@@ -20,5 +20,8 @@ float relevance(unsigned int *partition, unsigned int partition_size);
 // Knowing the degeneracy profile
 float relevance_degeneracy(unsigned int *dg_profile, unsigned int dg_profile_size);
                           
+// Energy of a lattice with boundary conditions
+float energy_lattice(unsigned int *lattice, unsigned int lattice_shape[2],
+                     char boundary_conditions = 'o');
 
 #endif // quantifying_information_h
